21d63ac28bd65597394aa8c2fd6b0a4878443b9c
toufiqnuur <taufik2017yk@gmail.com>
2022-12-11 19:41:52 +0700

    feat: expired link checker

--- src/pages/[id].js
+++ src/pages/[id].js
@@ -1,7 +1,21 @@
+import dayjs from "dayjs";
+import Container from "../components/Container";
+import Footer from "../components/Footer";
 import { supabase } from "../utils/supabase";
 
-export default function RedirectPage() {
-  return <></>;
+export default function RedirectPage(props) {
+  return (
+    <>
+      <Container className="py-40 text-center">
+        <h2 className="text-4xl font-bold">403 Forbidden</h2>
+        <p className="mt-4 text-lg">
+          {props?.url?.isExpired &&
+            "Tautan sudah kadaluwarsa hubungi pemilik jika ada kesalahan"}
+        </p>
+      </Container>
+      <Footer />
+    </>
+  );
 }
 
 export async function getServerSideProps({ params }) {
@@ -12,6 +26,19 @@ export async function getServerSideProps({ params }) {
     .single();
 
   if (data) {
+    const isExpired = dayjs(data.expired_at).valueOf() < dayjs().valueOf();
+
+    if (isExpired) {
+      return {
+        props: {
+          url: {
+            isExpired: true,
+            expired_at: data.expired_at,
+          },
+        },
+      };
+    }
+
     return {
       notFound: false,
       redirect: {
